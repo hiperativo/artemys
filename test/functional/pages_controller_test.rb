9ffd5e8548fe3d9e4aec95be05b829940be4b1b5
Pedro Maciel <pedrozath@gmail.com>
2012-09-19 14:02:56 -0300

    fiz um pouco de css e outras coisas

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get discover" do
+    get :discover
+    assert_response :success
+  end
+
+end
