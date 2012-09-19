9ee6a972565620340f341d22cc1093d2147a124e
Pedro Maciel <pedrozath@gmail.com>
2012-09-18 22:13:24 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artemys::Application.config.secret_token = 'df1b962e607352d9b954c21a20b8498d72455f15af6ead5b9738c52c11d095ef63bcd2eddca2e0b46beaa0975900663925df4b847a95509274f89a29bdc6cedd'
