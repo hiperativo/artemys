25bdc5519a7a59f8e76e594f17990d57787cad5f
Pedro Maciel <pedrozath@gmail.com>
2012-11-20 10:52:35 -0200

    added translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	before_filter :set_locale
+
+	def set_locale
+		unless params[:language].nil?
+			I18n.locale = params[:language]
+		end
+	end
 end
---
+++ config/initializers/copycopter.rb
@@ -0,0 +1,4 @@
+CopycopterClient.configure do |config|
+  config.api_key = '2d37449b01ac5bc6e417684fdc74dbe5d4eba6187c9649d2'
+  config.host = 'hipercopy.herokuapp.com'
+end
