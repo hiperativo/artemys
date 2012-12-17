8351f629a22dd45027e1a552532e725f34e9f8e3
Pedro Maciel <pedrozath@gmail.com>
2012-12-17 16:38:38 -0200

    where to buy desigado temporariamente

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,39 +12,39 @@ class PagesController < ApplicationController
 	def gallery
 	end
 
-	def where_to_buy
-		@lojas_venezuela ||= SmarterCSV.process (Rails.root.join "app", "assets", "csv", "lojas-venezuela.csv"), { col_sep:';'}
-		@states, @cities = {}
-		for store in @lojas_venezuela
-			unless @states[store[:state]]
-				@states[store[:state]] = {}
-				unless @states[store[:state]][store[:city]]
-					@states[store[:state]][store[:city]] = {}
-				end
-			end
-			@states[store[:state]][store[:city]] = store
-		end
-	end
+	# def where_to_buy
+	# 	@lojas_venezuela ||= SmarterCSV.process (Rails.root.join "app", "assets", "csv", "lojas-venezuela.csv"), { col_sep:';'}
+	# 	@states, @cities = {}
+	# 	for store in @lojas_venezuela
+	# 		unless @states[store[:state]]
+	# 			@states[store[:state]] = {}
+	# 			unless @states[store[:state]][store[:city]]
+	# 				@states[store[:state]][store[:city]] = {}
+	# 			end
+	# 		end
+	# 		@states[store[:state]][store[:city]] = store
+	# 	end
+	# end
 	
-	def places
-		@lojas_venezuela ||= SmarterCSV.process (Rails.root.join "app", "assets", "csv", "lojas-venezuela.csv"), { col_sep:';'}
+	# def places
+	# 	@lojas_venezuela ||= SmarterCSV.process (Rails.root.join "app", "assets", "csv", "lojas-venezuela.csv"), { col_sep:';'}
 		
-		@states, @cities = {}
-		for store in @lojas_venezuela
-			unless @states[store[:state]]
-				@states[store[:state]] = {}
-				unless @states[store[:state]][store[:city]]
-					@states[store[:state]][store[:city]] = {}
-				end
-			end
-			@states[store[:state]][store[:city]] = store
-		end
+	# 	@states, @cities = {}
+	# 	for store in @lojas_venezuela
+	# 		unless @states[store[:state]]
+	# 			@states[store[:state]] = {}
+	# 			unless @states[store[:state]][store[:city]]
+	# 				@states[store[:state]][store[:city]] = {}
+	# 			end
+	# 		end
+	# 		@states[store[:state]][store[:city]] = store
+	# 	end
 
-		respond_to do |format|
-			format.json { render :json => @states}
-			format.xml
-		end
-	end
+	# 	respond_to do |format|
+	# 		format.json { render :json => @states}
+	# 		format.xml
+	# 	end
+	# end
 
 	def contact
 	end
